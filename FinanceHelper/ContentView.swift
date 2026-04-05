55c16ca4fc494b25197dc7f3526346d45e980fe9
UTTAM KUMAR <rajuttam4439@gmail.com>
2026-04-05 21:01:12 +0530

    Initial Commit

---
+++ FinanceHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinanceHelper
+//
+//  Created by UTTAM KUMAR on 05/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinanceHelper/FinanceHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceHelperApp.swift
+//  FinanceHelper
+//
+//  Created by UTTAM KUMAR on 05/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
